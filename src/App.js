7132d45c776816c642549370aa376f4dab1ae09d
Magdalena Kałamarz <magda.kalamarz@wp.pl>
2018-09-28 23:20:44 +0200

    Add store

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import  store from "./store";
+import { Provider } from "react-redux";
 
 class App extends Component {
+
   render() {
     return (
-      <div>
-        
-      </div>
+      <Provider store={store}>
+        <div>
+
+        </div>
+      </Provider>
     );
   }
 }
