03e1a0ebc77a625b5d35ebdbf0011604b3ba96e1
Magdalena Kałamarz <magda.kalamarz@wp.pl>
2018-10-02 13:26:52 +0200

    Remove Overlay from CoastMap component

--- src/components/CoastMap.js
+++ src/components/CoastMap.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import Map from 'pigeon-maps';
 import Marker from 'pigeon-marker';
-import Overlay from 'pigeon-overlay';
+
 
 const CoastMap = props => (
   <Map center={props.center} zoom={12} width={600} height={400}>
-    <Marker anchor={props.center}/>
-
-    <Overlay anchor={props.center}>
-      <img src='pigeon.jpg' width={240} height={158} alt='map' />
-    </Overlay>
+    <Marker anchor={props.center} />
   </Map>
 )
 
