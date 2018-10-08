a291fd41a59615ce5b19474e9b75d7be7b93c714
Magdalena Kałamarz <magda.kalamarz@wp.pl>
2018-10-08 19:55:23 +0200

    Add Sidebar Component

--- src/App.js
+++ src/App.js
@@ -4,15 +4,50 @@ import Coasts from './components/Coasts';
 import CoastDetails from './components/CoastDetails';
 import  store from "./store";
 import { Provider } from "react-redux";
+import Sidebar from 'react-sidebar';
+
+const mql = window.matchMedia(`(min-width: 800px)`);
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      sidebarOpen: false,
+      sidebarDocked: mql.matches,
+    };
+  }
+
+   componentWillMount() {
+    mql.addListener(this.mediaQueryChanged);
+  }
+
+  componentWillUnmount() {
+    mql.removeListener(this.mediaQueryChanged);
+  }
+
+  onSetSidebarOpen = open => {
+    this.setState({ sidebarOpen: open });
+  }
+
+  mediaQueryChanged = () => {
+    this.setState({ sidebarDocked: mql.matches, sidebarOpen: false });
+  }
 
   render() {
     return (
       <Provider store={store}>
         <div>
-          <Coasts />
-          <CoastDetails />
+          <Sidebar
+            sidebar={ <Coasts /> }
+            open={this.state.sidebarOpen}
+            docked={this.state.sidebarDocked}
+            onSetOpen={this.onSetSidebarOpen}>
+            <button onClick={() => this.onSetSidebarOpen(true)}>
+              Show list
+            </button>
+            <CoastDetails />
+          </Sidebar>
         </div>
       </Provider>
     );
